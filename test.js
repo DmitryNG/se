8f8ccfaf515fead9381ec6ad0a38dd9d75e4ee39
Dmitry Nagibin <d.nagibin2014@yandex.ru>
2021-02-18 19:12:05 +0500

    testing...

--- test.js
+++ test.js
@@ -1,30 +1,45 @@
-let x = 0;
+readln = require('readline-sync'); // подключаем модуль
+
+let x = 6;
 let y = 55;
-
-var readline = require('readline');
-
-var rl = readline.createInterface(
-    {
-    input: process.stdin,
-    output: process.stdout
-})
-
-let lines = [];
-
-rl.on('line', function(line){
-    lines.push(line);
-});
 
 function f (i)
 {
-    if(i=='+')
-    {
-        console.log(`Сумма = ${a+b}`);
-    }
+    switch (i) {
+        case '+':
+            console.log(`Сумма =`, x+y);
+            break;
 
-    if(i=='-')
-    {
-        console.log(`Произведение = ${a+b}`);
-    }
+        case '-':
+                console.log(`Разность =`, x-y);
+                break;
+    
+        case '*':
+                console.log(`Произведение =`, x*y);
+                break; 
+        
+        case '/':
+                console.log(`Частное =`, x/y);
+                break;   
 
+        case 'exit':
+            console.log(`Выход...`);
+            break;   
+
+        default:
+            console.log('Фиг вам... Введите правильный оператор');
+            break;
+    }
 } 
+answer = String(readln.question('> ')); // читаем с консоли
+//console.log(answer);
+while(answer!=`exit`){
+    
+
+    console.log("Введите оператор (для выхода введите 'exit')");
+    answer = String(readln.question('> ')); // читаем с консоли
+    
+
+    f(answer);
+    //console.clear();
+}
